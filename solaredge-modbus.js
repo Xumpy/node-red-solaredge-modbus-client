ebfa3ddb9030e11eef53d061c14d338c858fc08a
xumpy <nico.martens1985@gmail.com>
2020-03-19 10:18:47 +0100

    Trying to add a poller

--- solaredge-modbus.js
+++ solaredge-modbus.js
@@ -1,5 +1,6 @@
 let SolarEdgeModbusClient = require('solaredge-modbus-client')
 const fs = require('fs');
+const poller = require('./poller');
 
 /*
 let solar = new SolarEdgeModbusClient({
@@ -13,9 +14,33 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            //msg.payload = solar.getData();
-            msg.payload = JSON.parse(fs.readFileSync('mock.json'));
-            node.send(msg);
+			let poller = new Poller(1000);
+			poller.onPoll(() => {
+				const RELAVENT_DATA = [
+					'C_Manufacturer',
+					'C_Model',
+					'C_Version',
+					'C_SerialNumber',
+					'I_AC_Current',
+					'I_AC_VoltageAB',
+					'I_AC_Power',
+					'I_AC_Energy_WH',
+					'I_DC_Current',
+					'I_DC_Voltage',
+					'I_DC_Power',
+					'I_Temp_Sink'
+				];
+				
+				let results = [];
+				
+				//msg.payload = solar.getData();
+				msg.payload = JSON.parse(fs.readFileSync('mock.json'));
+				node.send(msg);
+				poller.poll(); // Go for the next poll
+			});
+
+			// Initial start
+			poller.poll();
         });
     }
     RED.nodes.registerType("solaredge-modbus",getModbusData);
