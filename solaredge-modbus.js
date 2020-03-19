f25230c2f16b8f3a197c5e9ec3fa2f7c46ea8a92
xumpy <nico.martens1985@gmail.com>
2020-03-19 11:17:57 +0100

    Test

--- solaredge-modbus.js
+++ solaredge-modbus.js
@@ -13,8 +13,28 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
 		setInterval(() => {
+			const RELAVENT_DATA = [
+					'',
+					'C_Model',
+					'C_Version',
+					'C_SerialNumber',
+					'I_AC_Current',
+					'I_AC_VoltageAB',
+					'I_AC_Power',
+					'I_AC_Energy_WH',
+					'I_DC_Current',
+					'I_DC_Voltage',
+					'I_DC_Power',
+					'I_Temp_Sink'
+				];
+				
+			let results = [];
+			
+			modbus = JSON.parse(fs.readFileSync('mock.json'));
 			var msg = {
-				payload: "started"
+				payload: {
+					'C_Manufacturer': modbus['C_Manufacturer']
+				}
 			};
 			node.send(msg);
 		}, 1000);
