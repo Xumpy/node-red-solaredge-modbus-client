f9f2c498c094b3e4c2afb140af1a0a580595361e
xumpy <nico.martens1985@gmail.com>
2020-03-19 09:01:24 +0100

    First draft version of reading a mock of solaredge data and putting it on the payload

---
+++ solaredge-modbus.js
@@ -0,0 +1,22 @@
+let SolarEdgeModbusClient = require('solaredge-modbus-client')
+const fs = require('fs');
+
+/*
+let solar = new SolarEdgeModbusClient({
+    host: "192.168.1.200",
+    port: 1502
+})
+*/
+
+module.exports = function(RED) {
+    function getModbusData(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            //msg.payload = solar.getData();
+            msg.payload = fs.readFileSync('mock.json');
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("solaredge-modbus",getModbusData);
+}
