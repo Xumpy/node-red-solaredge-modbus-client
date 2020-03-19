0e912eda9b2efa5bb78a8ce951e376215c394221
xumpy <nico.martens1985@gmail.com>
2020-03-19 12:14:41 +0100

    Create map with docker consistent keys

--- solaredge-modbus.js
+++ solaredge-modbus.js
@@ -17,27 +17,29 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
 		setInterval(() => {
-			const RELAVENT_DATA = [
-					'',
-					'C_Model',
-					'C_Version',
-					'C_SerialNumber',
-					'I_AC_Current',
-					'I_AC_VoltageAB',
-					'I_AC_Power',
-					'I_AC_Energy_WH',
-					'I_DC_Current',
-					'I_DC_Voltage',
-					'I_DC_Power',
-					'I_Temp_Sink'
-				];
-				
-			let results = [];
-			
 			modbus = JSON.parse(fs.readFileSync('mock.json'));
 			var msg = {
 				payload: {
-					'C_Manufacturer': fetchValue(modbus, 'I_AC_Energy_WH')
+					'Model': fetchValue(modbus, 'C_Model'),
+					'Version': fetchValue(modbus, 'C_Version'),
+					'SerialNumbeer': fetchValue(modbus, 'C_SerialNumber'),
+					'AC Current phase A': fetchValue(modbus, 'I_AC_CurrentA'),
+					'AC Current phase B': fetchValue(modbus, 'I_AC_CurrentB'),
+					'AC Current phase C': fetchValue(modbus, 'I_AC_CurrentC'),
+					'AC Frequency': fetchValue(modbus, 'I_AC_Frequency'),
+					'AC Power output': fetchValue(modbus, 'I_AC_Power'),
+					'AC Total Current': fetchValue(modbus, 'I_AC_Current'),
+					'AC Voltage phase A-B': fetchValue(modbus, 'I_AC_VoltageAB'),
+					'AC Voltage phase A-N': fetchValue(modbus, 'I_AC_VoltageAN'),
+					'AC Voltage phase B-C': fetchValue(modbus, 'I_AC_VoltageBC'),
+					'AC Voltage phase B-N': fetchValue(modbus, 'I_AC_VoltageBN'),
+					'AC Voltage phase C-A': fetchValue(modbus, 'I_AC_VoltageCA'),
+					'AC Voltage phase C-N': fetchValue(modbus, 'I_AC_VoltageCN'),
+					'AC Energy Total WH': fetchValue(modbus, 'I_AC_Energy_WH'),
+					'DC Current': fetchValue(modbus, 'I_DC_Current'),
+					'DC Power input': fetchValue(modbus, 'I_DC_Power'),
+					'DC Voltage': fetchValue(modbus, 'I_DC_Voltage'),
+					'Inverter Temperature':fetchValue(modbus, 'I_Temp_Sink')
 				}
 			};
 			node.send(msg);
