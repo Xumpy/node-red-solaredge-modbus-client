39457ed6e98841f2156b2b02885b22bad259a782
xumpy <nico.martens1985@gmail.com>
2020-03-19 13:42:32 +0100

    Test

--- solaredge-modbus.js
+++ solaredge-modbus.js
@@ -23,26 +23,26 @@ module.exports = function(RED) {
 			modbus = solar.getData().then((data) => {
 				var msg = {
 					payload: {
-						'Model': fetchValue(modbus, 'C_Model'),
-						'Version': fetchValue(modbus, 'C_Version'),
-						'SerialNumbeer': fetchValue(modbus, 'C_SerialNumber'),
-						'AC Current phase A': numberWithScale(fetchValue(modbus, 'I_AC_CurrentA'), fetchValue(modbus, 'I_AC_Current_SF')),
-						'AC Current phase B': numberWithScale(fetchValue(modbus, 'I_AC_CurrentB'), fetchValue(modbus, 'I_AC_Current_SF')),
-						'AC Current phase C': numberWithScale(fetchValue(modbus, 'I_AC_CurrentC'), fetchValue(modbus, 'I_AC_Current_SF')),
-						'AC Total Current': numberWithScale(fetchValue(modbus, 'I_AC_Current'), fetchValue(modbus, 'I_AC_Current_SF')),
-						'AC Frequency': numberWithScale(fetchValue(modbus, 'I_AC_Frequency'), fetchValue(modbus, 'I_AC_Frequency_SF')),
-						'AC Power output': numberWithScale(fetchValue(modbus, 'I_AC_Power'), fetchValue(modbus, 'I_AC_Power_SF')),
-						'AC Voltage phase A-B': numberWithScale(fetchValue(modbus, 'I_AC_VoltageAB'), fetchValue(modbus, 'I_AC_Voltage_SF')),
-						'AC Voltage phase A-N': numberWithScale(fetchValue(modbus, 'I_AC_VoltageAN'), fetchValue(modbus, 'I_AC_Voltage_SF')),
-						'AC Voltage phase B-C': numberWithScale(fetchValue(modbus, 'I_AC_VoltageBC'), fetchValue(modbus, 'I_AC_Voltage_SF')),
-						'AC Voltage phase B-N': numberWithScale(fetchValue(modbus, 'I_AC_VoltageBN'), fetchValue(modbus, 'I_AC_Voltage_SF')),
-						'AC Voltage phase C-A': numberWithScale(fetchValue(modbus, 'I_AC_VoltageCA'), fetchValue(modbus, 'I_AC_Voltage_SF')),
-						'AC Voltage phase C-N': numberWithScale(fetchValue(modbus, 'I_AC_VoltageCN'), fetchValue(modbus, 'I_AC_Voltage_SF')),
-						'AC Total Energy WH': numberWithScale(fetchValue(modbus, 'I_AC_Energy_WH'), fetchValue(modbus, 'I_AC_Energy_WH_SF')),
-						'DC Current': numberWithScale(fetchValue(modbus, 'I_DC_Current'), fetchValue(modbus, 'I_DC_Current_SF')),
-						'DC Power input': numberWithScale(fetchValue(modbus, 'I_DC_Power'), fetchValue(modbus, 'I_DC_Power_SF')),
-						'DC Voltage': numberWithScale(fetchValue(modbus, 'I_DC_Voltage'), fetchValue(modbus, 'I_DC_Voltage_SF')),
-						'Inverter Temperature':numberWithScale(fetchValue(modbus, 'I_Temp_Sink'), fetchValue(modbus, 'I_Temp_SF'))
+						'Model': fetchValue(data, 'C_Model'),
+						'Version': fetchValue(data, 'C_Version'),
+						'SerialNumbeer': fetchValue(data, 'C_SerialNumber'),
+						'AC Current phase A': numberWithScale(fetchValue(data, 'I_AC_CurrentA'), fetchValue(data, 'I_AC_Current_SF')),
+						'AC Current phase B': numberWithScale(fetchValue(data, 'I_AC_CurrentB'), fetchValue(data, 'I_AC_Current_SF')),
+						'AC Current phase C': numberWithScale(fetchValue(data, 'I_AC_CurrentC'), fetchValue(data, 'I_AC_Current_SF')),
+						'AC Total Current': numberWithScale(fetchValue(data, 'I_AC_Current'), fetchValue(data, 'I_AC_Current_SF')),
+						'AC Frequency': numberWithScale(fetchValue(data, 'I_AC_Frequency'), fetchValue(data, 'I_AC_Frequency_SF')),
+						'AC Power output': numberWithScale(fetchValue(data, 'I_AC_Power'), fetchValue(data, 'I_AC_Power_SF')),
+						'AC Voltage phase A-B': numberWithScale(fetchValue(data, 'I_AC_VoltageAB'), fetchValue(data, 'I_AC_Voltage_SF')),
+						'AC Voltage phase A-N': numberWithScale(fetchValue(data, 'I_AC_VoltageAN'), fetchValue(data, 'I_AC_Voltage_SF')),
+						'AC Voltage phase B-C': numberWithScale(fetchValue(data, 'I_AC_VoltageBC'), fetchValue(data, 'I_AC_Voltage_SF')),
+						'AC Voltage phase B-N': numberWithScale(fetchValue(data, 'I_AC_VoltageBN'), fetchValue(data, 'I_AC_Voltage_SF')),
+						'AC Voltage phase C-A': numberWithScale(fetchValue(data, 'I_AC_VoltageCA'), fetchValue(data, 'I_AC_Voltage_SF')),
+						'AC Voltage phase C-N': numberWithScale(fetchValue(data, 'I_AC_VoltageCN'), fetchValue(data, 'I_AC_Voltage_SF')),
+						'AC Total Energy WH': numberWithScale(fetchValue(data, 'I_AC_Energy_WH'), fetchValue(data, 'I_AC_Energy_WH_SF')),
+						'DC Current': numberWithScale(fetchValue(data, 'I_DC_Current'), fetchValue(data, 'I_DC_Current_SF')),
+						'DC Power input': numberWithScale(fetchValue(data, 'I_DC_Power'), fetchValue(data, 'I_DC_Power_SF')),
+						'DC Voltage': numberWithScale(fetchValue(data, 'I_DC_Voltage'), fetchValue(data, 'I_DC_Voltage_SF')),
+						'Inverter Temperature':numberWithScale(fetchValue(data, 'I_Temp_Sink'), fetchValue(data, 'I_Temp_SF'))
 					}
 				};
 				node.send(msg);
